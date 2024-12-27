31c2ad16fb02ae469c46d51534cdec3f2067d835
Кирилл <frosiatyan@gmail.com>
2024-12-27 09:17:21 +0300

    Задание

--- greetings.py
+++ greetings.py
@@ -1,1 +1,12 @@
-
+username = "Кирилл Фролов"
+print("Имя пользователя: " + username)
+title = "Аниме"
+print("Заголовок заметки: " + title)
+content = "Посмотреть наруто со всеми филлерами"
+print("Описание заголовка: " + content)
+status = "В работе"
+print("Статус заметки: " + status)
+created_date = "27-12-2024"
+print("Дата создания: " + created_date)
+issue_date = "27.01.2025"
+print("Дата истечения заметки: " + issue_date)
