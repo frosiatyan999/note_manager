f6bb04bc33eaa793cb25b83e13c247b72f6f5b73
Кирилл <frosiatyan@gmail.com>
2025-01-02 05:00:56 +0300

    Задание

--- date_changer.py
+++ date_changer.py
@@ -1,5 +1,5 @@
-created_date = "28 декабря 2024"
-issue_date = "28 января 2024"
-print("Дата создания: " + (created_date[0:2] + "-12-" + created_date[11:15]))
-print("Дата окончания: " + (created_date[0:2] + "-01-" + created_date[11:15]))
+created_date = "28-12-2024"
+issue_date = "28-01-2024"
+print("Дата создания: " + created_date[0:5])
+print("Дата окончания: " + issue_date[0:5])
 
