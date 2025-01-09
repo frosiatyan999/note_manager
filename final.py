9ca107101e6a05e23fb812f38de301d29d3d370f
Kirill <mega.ha.ha@mail.ru>
2025-01-09 21:33:12 +0300

    Update final.py

--- final.py
+++ final.py
@@ -1,8 +1,7 @@
-note = [
-    input("Имя пользователя: "),
-    input("Содержание заметки: "),
-    input("Статус: "),
-    input("Дата создания: "),
-    [input("Заголовок 1: "), input("Заголовок 2: "), input("Заголовок 3: ")],
-]
+username = input("Имя пользователя: "),
+content = input("Содержание заметки: "),
+status = input("Статус: "),
+created_date = input("Дата создания: "),
+titles = [input("Заголовок 1: "), input("Заголовок 2: "), input("Заголовок 3: ")]
+note = [username, content, status, created_date, titles]
 print(note)
