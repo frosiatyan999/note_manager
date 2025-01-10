fdda02bc4b4f7257da9d0fda367187213472cd43
Кирилл <frosiatyan@gmail.com>
2025-01-10 19:24:01 +0300

    Задание

--- update_status.py
+++ update_status.py
@@ -1,8 +1,20 @@
 status = ['1.В процессе', '2.Отложено','3.Выполнено']
-status_0 = ['В процессе', 'Отложено','Выполнено']
-print('Текущий статус заметки: ' + status_0[0])
+print('Текущий статус заметки: ' + status[0])
 print('Выберите новое значение для статуса:')
 print('\n'.join(status))
-choise = ('Напишите число для изменения статуса заметки: ')
-print(input(choise))
-if choise == 1
+while True:
+    choise = print(input('Напишите число для изменения статуса заметки: '))
+    if choise == '1':
+        print('Текущий статус заметки: ' + status[0])
+        break
+    elif choise == '2':
+        print('Текущий статус заметки: ' + status[1])
+        break
+    elif choise == '3':
+        print('Текущий статус заметки: ' + status[2])
+        break
+    else:
+        print('Неверный ввод')
+        break
+
+
