135910aa07485bcee8575208bf4d21d79242a4dd
Kirill <mega.ha.ha@mail.ru>
2025-01-12 03:54:11 +0300

    Update multiple_notes.py

--- multiple_notes.py
+++ multiple_notes.py
@@ -1,1 +1,13 @@
-
+from datetime import datetime as dt
+multiple_notes = []
+print('Добро пожаловать')
+current_date = dt.today().date()
+print('Сегодня: ', current_date)
+while True:
+    welcome = input('Хотите создать заметку? да/нет').strip().lower()
+    if welcome.strip().lower() == 'да':
+        break
+    elif welcome.strip().lower() == 'нет':
+        quit('До встречи!')
+    else: 
+        print('Неверное значение')
