4dfc1c8a60599445565737b857d43455bf9626f3
Кирилл <frosiatyan@gmail.com>
2025-01-13 21:42:01 +0300

    Задание

--- multiple_notes.py
+++ multiple_notes.py
@@ -4,10 +4,26 @@ print('Добро пожаловать')
 current_date = dt.today().date()
 print('Сегодня: ', current_date)
 while True:
-    welcome = input('Хотите создать заметку? да/нет').strip().lower()
+    welcome = input('Хотите создать заметку? да/нет\n').strip().lower()
     if welcome.strip().lower() == 'да':
         break
     elif welcome.strip().lower() == 'нет':
         quit('До встречи!')
     else: 
         print('Неверное значение')
+i = 0
+while True:
+    print('Введите данные для заметки{i \\]}')
+    note = {}
+    while True:
+        username = input('Введите ваше имя').strip().lower()
+        # Пишем условие которое должно выполняться что бы остановить добавление заголовков а так же добавление заголовков в список
+        if username.lower() == '':
+            print('Вы не добавили имя!')
+            continue
+        else:
+            note['Имя'] = username
+            break
+
+
+
