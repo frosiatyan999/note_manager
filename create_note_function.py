4ea9519c2fdc5e083c464960bae4b48de85f94b6
Кирилл <frosiatyan@gmail.com>
2025-01-16 04:25:02 +0300

    Задание

--- create_note_function.py
+++ create_note_function.py
@@ -1,5 +1,9 @@
 # Загружаем библиотеку datetime для работы с датами
 from datetime import datetime as dt
+from datetime import timedelta
+
+from date_changer import issue_date, created_date
+
 
 # Создаём функцию создания заметки
 def create_note():
@@ -10,16 +14,23 @@ def create_note():
     status = input('Введите статус заметки ("новая", "в процессе", "выполнена"): ').lower().strip()
 # Текущая дата вводится автоматически
     created_date = dt.now().date().strftime('%d-%m-%Y')
-# С помощью цикла вводим и проверяем правильность даты дедлайна\
+# С помощью цикла вводим и проверяем правильность даты дедлайна
     while True:
-        issue_date = input('Введите дату дедлайна заметки\n'
-                    ' в формате "дд-мм-гггг"(через дефис и без пробелов): ')
-        try:
-            issue_date = dt.strptime(issue_date, '%d-%m-%Y').date()
+        issue_date = input('Введите дату дедлайна заметкив формате "дд-мм-гггг"\n'
+                    ' (через дефис и без пробелов): или выберите по умолчанию нажав Enter ')
+        if issue_date == '':
+            created_date = dt.strptime(created_date, '%d-%m-%Y').date()
+            issue_date = created_date + timedelta(days=7)
             break
-        except ValueError:
-            print('Неверный ввод! Попробуйте ещё.')
-            continue
+        else:
+            try:
+                issue_date = dt.strptime(issue_date, '%d-%m-%Y').date()
+                break
+            except ValueError:
+                print('Неверный ввод! Попробуйте ещё.')
+                continue
+
+
 # Записываем все полученные данные в словарь
     note = {
         "username": username,
