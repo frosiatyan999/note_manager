54ea4f33aa1c0bd0f22249320534ee0ae3032218
Кирилл <frosiatyan@gmail.com>
2025-01-17 23:16:19 +0300

    Задание

--- delete_note.py
+++ delete_note.py
@@ -21,7 +21,8 @@ for item in note_list:
 while True:
     choice_title = input('Введите имя пользователя или заголовок заметки которую хотите удалить: \n').lower().strip()
     for j in reversed(range(len(note_list))):
-        if note_list[j]['Заголовок:'].lower().strip() != choice_title and note_list[j]['Имя:'] != choice_title:
+        if (note_list[j]['Заголовок:'].lower().strip() != choice_title
+                and note_list[j]['Имя:'] != choice_title):
             continue
 # Когда заголовки совпали, предлагается подтвердить удаление
         elif note_list[j]['Заголовок:'].lower().strip() == choice_title or note_list[j]['Имя:'] == choice_title:
@@ -39,14 +40,15 @@ while True:
 # Если заголовок не найден, программа об этом сообщает либо после удаления таких заметок нет
         print('Заметок для удаления не найдено!')
         continue
+    stop_ = input('\nВведите стоп если хотите закончить удаление: \n').lower().strip()
 # Выводим список заметок после изменения
     print('Ваш список заметок после изменения:\n')
     b = 0
+
     for item in note_list:
         print(f'Заметка №{b + 1}:')
         b += 1
         print(*item.items(), sep='\n')
-    stop_ = input('\nВведите стоп если хотите закончить удаление: \n').lower().strip()
     if stop_.lower() == "стоп":
         break
     continue
